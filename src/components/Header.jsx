efe84c9ca08985255205860573ee70d764fd584b
lyjacky11 <lyjacky@live.com>
2020-11-03 12:03:20 -0500

    Theme fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faAdjust } from '@fortawesome/free-solid-svg-icons';
 
 class Header extends Component {
-    
+
     toggleTheme() {
         document.querySelector("body").classList.toggle("light");
     }
@@ -14,7 +14,7 @@ class Header extends Component {
             <div className="HeaderComponent">
                 <h3>Discover Asian Dramas</h3>
                 <div className="headerElements">
-                    <Link className="homeLink" to="/"><h4><FontAwesomeIcon icon={faHome} />&nbsp; Home</h4></Link>
+                    <Link className="homeLink" to="/"><h5><FontAwesomeIcon icon={faHome} />&nbsp; Home</h5></Link>
                     <FontAwesomeIcon className="themeBtn" icon={faAdjust} size="2x" onClick={this.toggleTheme} />
                 </div>
             </div>
