7b3e6f8e38c893b9d6bf60d963be3734a5e9543e
lyjacky11 <lyjacky@live.com>
2020-11-03 12:23:51 -0500

    Add "toggle theme" text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,13 +6,14 @@ import { faHome, faAdjust } from '@fortawesome/free-solid-svg-icons';
 class Header extends Component {
     render() {
         const { toggleTheme } = this.props;
-        
+
         return (
             <div className="HeaderComponent">
                 <h3>Discover Asian Dramas</h3>
                 <div className="headerElements">
                     <Link className="homeLink" to="/"><h5><FontAwesomeIcon icon={faHome} />&nbsp; Home</h5></Link>
                     <FontAwesomeIcon className="themeBtn" icon={faAdjust} size="2x" onClick={toggleTheme} />
+                    <span className="themeText" onClick={toggleTheme}>&nbsp; <b>Toggle Theme</b></span>
                 </div>
             </div>
 
