306b867c31949ded354f8c3db220f506204e06cb
lyjacky11 <lyjacky@live.com>
2020-11-03 17:45:56 -0500

    Change header/footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,11 @@ import logo from '../TMDB.svg';
 
 class Footer extends Component {
     render() {
-        const currentYear = new Date().getFullYear();
         return (
             <div className="FooterComponent">
-                <h5>The Movie Database (TMDb):</h5>
-                <a href="https://www.themoviedb.org" target="_blank" rel="noreferrer"><img alt="TMDB Logo" src={logo} width="150"></img></a>
-                <h6 className="mt-4">© Copyright {currentYear}, <a href="https://github.com/lyjacky11" target="_blank" rel="noreferrer">Jacky Ly.</a></h6>
+                <h6>Data and title information from:</h6>
+                <p><a href="https://www.themoviedb.org" target="_blank" rel="noreferrer"><img alt="TMDB Logo" src={logo} width="150"></img></a></p>
+                <h6>The Movie Database (TMDb)</h6>
             </div>
         );
     }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,10 @@ import { faHome, faAdjust } from '@fortawesome/free-solid-svg-icons';
 class Header extends Component {
     render() {
         const { toggleTheme } = this.props;
-
         return (
             <div className="HeaderComponent">
                 <h3>Discover Asian Dramas</h3>
+                <h6 className="mt-4">Designed and developed by: <a href="https://github.com/lyjacky11" target="_blank" rel="noreferrer">Jacky Ly</a></h6>
                 <div className="headerElements">
                     <Link className="homeLink" to="/"><h5><FontAwesomeIcon icon={faHome} />&nbsp; Home</h5></Link>
                     <FontAwesomeIcon className="themeBtn" icon={faAdjust} size="2x" onClick={toggleTheme} />
