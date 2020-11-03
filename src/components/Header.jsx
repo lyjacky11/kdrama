09cb716f1666c12414222e0f470b99d07eef9792
lyjacky11 <lyjacky@live.com>
2020-11-03 11:54:47 -0500

    Added theme toggle button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faAdjust } from '@fortawesome/free-solid-svg-icons';
 
 class Header extends Component {
+    
+    toggleTheme() {
+        document.querySelector("body").classList.toggle("light");
+    }
+
     render() {
         return (
             <div className="HeaderComponent">
                 <h3>Discover Asian Dramas</h3>
-                <h4 className="homeLink">
-                    <Link to="/"><FontAwesomeIcon icon={faHome} />&nbsp; Home</Link>
-                </h4>
+                <div className="headerElements">
+                    <Link className="homeLink" to="/"><h4><FontAwesomeIcon icon={faHome} />&nbsp; Home</h4></Link>
+                    <FontAwesomeIcon className="themeBtn" icon={faAdjust} size="2x" onClick={this.toggleTheme} />
+                </div>
             </div>
 
         );
